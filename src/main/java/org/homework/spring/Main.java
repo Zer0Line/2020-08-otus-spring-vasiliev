d974cbf9536db4a4ac3dc9593b86d4b2f7d6cce6
Sergey <egorcev.s@noone.ru>
2020-11-29 17:14:01 +0300

    project prepare

--- src/main/java/org/homework/spring/Main.java
+++ src/main/java/org/homework/spring/Main.java
@@ -1,12 +1,10 @@
 package org.homework.spring;
 
-import org.homework.spring.config.AppProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProps.class)
 class Main {
 
     public static void main(String[] args) {
